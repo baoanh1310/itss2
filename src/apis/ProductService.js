bf11c673a136b26fbe3f2fae16aa82f14f32bd23
baoanh1310 <dataholicguy@gmail.com>
2021-11-29 02:18:32 +0700

    Add raw ProductModal

--- src/apis/ProductService.js
+++ src/apis/ProductService.js
@@ -29,7 +29,7 @@ class ProductService extends BaseService {
     }
 
     delProducts = (data) => {
-        return this.post("/delete", data);
+        return this.delete("/delete", data);
     }
 };
 
--- src/components/Content.js
+++ src/components/Content.js
@@ -85,7 +85,7 @@ const AddBtn = (props) => {
 	let target = "#".concat(modalId)
 	return (
 		<div>
-			<button className="btn btn-primary add-btn" style={{marginTop: "40px"}} data-toggle="modal" data-target={target} onClick={() => setModalShow(true)} > 
+			<button className="btn btn-success add-btn" style={{marginTop: "40px"}} data-toggle="modal" data-target={target} onClick={() => setModalShow(true)} > 
 				{btnText}
 			</button>
 			{modal}
@@ -259,10 +259,51 @@ const SupplierModal = (props) => {
 }
 
 const ProductModal = (props) => {
-	return (
-		<div>
+	const [productName, setProductName] = useState('')
+	const [supplierName, setSupplierName] = useState('')
+	const [productQuantity, setProductQuantity] = useState(1)
 
-		</div>
+	const handleSupplierNameChange = (e) => {
+		setSupplierName(e.target.value)
+	}
+
+	const handleProductNameChange = (e) => {
+		setProductName(e.target.value)
+	}
+
+	const handleProductQuantityChange = (e) => {
+		let quantity = parseInt(e.target.value)
+		setProductQuantity(quantity)
+	}
+
+	const handleCreateProduct = (e) => {
+		e.preventDefault()
+		console.log("Created new product")
+	}
+	return (
+		<div className="modal fade" id={props.modalId} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		  <div className="modal-dialog modal-dialog-centered" role="document">
+		    <div className="modal-content">
+		      <div className="modal-header">
+		        <h5 className="modal-title" id="exampleModalLabel">新製品</h5>
+		        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div className="modal-body">
+		        <form action="/products" method="POST">
+		        	<input value={productName} onChange={handleProductNameChange} type="text" className="form-control validate" placeholder="製品名" />
+		        	<input value={supplierName} onChange={handleSupplierNameChange} type="text" className="form-control validate" placeholder="サプライヤー名" />
+		        	<input value={productQuantity} onChange={handleProductQuantityChange} type="number" min="1" max="10000" className="form-control validate" placeholder="製品数" />
+		        </form>
+		      </div>
+		      <div className="modal-footer">
+		        <button type="button" className="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+		        <button type="submit" className="btn btn-primary" onClick={handleCreateProduct} >追加</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>			
 	)
 }
 
