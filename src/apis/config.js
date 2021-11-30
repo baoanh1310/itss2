9f729dc22e9c3476513667720d8b140b15c71d38
baoanh1310 <dataholicguy@gmail.com>
2021-11-30 10:45:45 +0700

    Change API url

--- src/apis/config.js
+++ src/apis/config.js
@@ -4,7 +4,8 @@ const envs = {
     production: "production"
 };
 // const endpoint = "https://quan-ly-kho-api.vercel.app/api/";
-const endpoint = "https://quan-ly-kho.phamhuuan.repl.co/api/";
+// const endpoint = "https://quan-ly-kho.phamhuuan.repl.co/api/";
+const endpoint = "https://quanlykho123.herokuapp.com/api/"
 const timeout = 50000;
 
 export { endpoint, timeout };
