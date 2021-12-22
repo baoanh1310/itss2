934184cecf392e7989cd49770b3be9a20c66e7a2
baoanh1310 <dataholicguy@gmail.com>
2021-12-23 06:33:37 +0700

    add outOfStock and almostOutOfStock columns in Dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,22 +4,24 @@ const Dashboard = (props) => {
 
     let numberSupplier = props.suppliers.length;
     let numberProductCategories = props.products.length;
-    let numberOutOfStock = props.products.filter(product => product.amount < 10).length;
+    let numberAlmostOutOfStock = props.products.filter(product => product.amount < 10 && product.amount > 0).length;
+    let numberOutOfStock = props.products.filter(product => product.amount == 0).length;
 
     return (
         <div style={{display: "flex", marginTop: "20px"}}>
             <Card label="製品の種類の数" text={numberProductCategories} />
             <Card label="サプライヤーの数" text={numberSupplier} />
-            <Card label="商品の在庫がなくなりそうです" text={numberOutOfStock} />
+            <Card label="商品の在庫がなくなりそうだ" text={numberAlmostOutOfStock} />
+            <Card label="商品数量が在庫切れ" text={numberOutOfStock} />
         </div>
     )
 }
 
 const Card = (props) => {
     return (
-        <div className="card col-sm-4">
+        <div className="card col-sm-3">
             <div className="card-body">
-                <h3 className="card-title text-center">{props.label}</h3>
+                <h4 className="card-title text-center">{props.label}</h4>
                 <h1 className="card-text text-center">{props.text}</h1>
             </div>
         </div>
