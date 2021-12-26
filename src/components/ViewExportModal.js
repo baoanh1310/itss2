f583df79ec3ee290d9dc6563354e7e9d6fb15d67
baoanh1310 <dataholicguy@gmail.com>
2021-12-26 10:39:59 +0700

    View export bill detail

--- src/components/ViewExportModal.js
+++ src/components/ViewExportModal.js
@@ -1,19 +1,18 @@
 import React from "react"
-import ExportBillService from "../apis/ExportBillService"
 
 const ViewExportModal = (props) => {
 
     let productExport = props.productExport
-    let exportID = productExport._id
-    let code = productExport.code
-    console.log("Export ID: ", exportID)
-    
-    const handleViewExportBill = async (e) => {
-		e.preventDefault()
-        
-        const res = await ExportBillService.getProduct(exportID)
-        
-	}
+    let warehouse = productExport.exportWarehouse;
+    let renderWarehouse = warehouse.map(
+        (product, i) => 
+            <tr key={product._id}>
+                <td className="text-center" scope="row">{i+1}</td>
+                <td className="text-center" scope="row">{product.product.name}</td>
+                <td className="text-center" scope="row">{product.product.supplier.name}</td>
+                <td className="text-center" scope="row">{product.amount}</td>
+            </tr>
+    )
 
     return (
         <div className="modal fade bd-example-modal-lg info-export" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -25,9 +24,23 @@ const ViewExportModal = (props) => {
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
+
                     <div className="modal-body">
-                        {code}
+                        <table className="table table-striped table-bordered table-fixed" style={{width: "100%"}}>
+                            <thead>
+                                <tr>
+                                    <th className="text-center" scope="col" style={{width: "10%"}}>#</th>
+                                    <th className="text-center" scope="col" style={{width: "25%"}}>製品名</th>
+                                    <th className="text-center" scope="col" style={{width: "25%"}}>サプライヤー名</th>
+                                    <th className="text-center" scope="col" style={{width: "10%"}}>数</th>
+                                </tr>	
+                            </thead>	
+                            <tbody>
+                                {renderWarehouse}
+                            </tbody>
+                        </table>
                     </div>
+
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">キャンセル</button>
                     </div>
