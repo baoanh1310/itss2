1429fb9e81b3ef884bc0e5a2581a18b6ea0fc7e5
baoanh1310 <dataholicguy@gmail.com>
2022-01-01 17:06:23 +0700

    Add required fields in import modal but still not fit the requirements

--- src/components/ImportModal.js
+++ src/components/ImportModal.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react"
-import ImportService from "../apis/ImportService"
+import ImportBillService from "../apis/ImportBillService"
 
 const ImportModal = (props) => {
 
-	const [productName, setProductName] = useState("Milk")
+	const [billCode, setBillCode] = useState("")
+	const [supplierName, setSupplierName] = useState("")
+	const [productName, setProductName] = useState("")
 	const [productQuantity, setProductQuantity] = useState(1)
 	const [importDate, setImportDate] = useState('')
+	const [price, setPrice] = useState(1)
+	const [filteredProducts, setFilteredProducts] = useState([])
+	
+	const [dataArr, setDataArr] = useState([])
 
 	console.log("Product name: ", productName)
 	console.log("Date: ", importDate)
@@ -15,6 +21,20 @@ const ImportModal = (props) => {
 		setProductName(e.target.value)
 	}
 
+	const handleSupplierNameChange = (e) => {
+		setSupplierName(e.target.value)
+		let filteredProducts = props.products.filter(product => product.supplierName === e.target.value)
+		setFilteredProducts(filteredProducts)
+	}
+
+	const handleBillCodeChange = (e) => {
+		setBillCode(e.target.value)
+	}
+
+	const handleProductPriceChange = (e) => {
+		let price = parseInt(e.target.value)
+		setPrice(price)
+	}
 
 	const handleProductQuantityChange = (e) => {
 		let quantity = parseInt(e.target.value)
@@ -37,20 +57,44 @@ const ImportModal = (props) => {
 	const handleCreateProductImport = async (e) => {
 		e.preventDefault()
 		// console.log("Created new product import")
+		const supplierId = props.suppliers.filter(supplier => supplier.name === supplierName)[0]._id
 		const productId = props.products.filter(product => product.name === productName)[0]._id
-		const data = {
-			productId: productId,
-			amount: productQuantity,
-			time: convertStringToDate(importDate)
+		console.log("ProductID: ", productId)
+		console.log("SupplierID: ", supplierId)
+		let data = []
+		let productObj = {
+			"productId": productId,
+			"amount": productQuantity,
+			"price": price
 		}
-		const res = await ImportService.create(data)
-		if (res.status === 201) {
-			window.location.reload()
+		data.push(productObj)
+		const req = {
+			code: billCode,
+			time: convertStringToDate(importDate),
+			supplierId: supplierId,
+			data: data
 		}
+		console.log(req.data)
+		await ImportBillService.create(req)
+			.then(res => {
+				if (res.status == 201) {
+					window.location.reload()
+				}
+			})
+			.catch(err => {
+				console.log(err)
+				alert("Create new bill error")
+			})
 	}
 
+	let supplierOptions = props.suppliers.map(
+		(supplier, i) => 
+			<option key={supplier._id} value={supplier.name}>
+				{supplier.name}
+			</option>
+	)
 
-	let productOptions = props.products.map(
+	let productOptions = filteredProducts.map(
 		(product, i) => 
 			<option key={product._id} value={product.name}>
 				{product.name}
@@ -68,7 +112,28 @@ const ImportModal = (props) => {
 		        </button>
 		      </div>
 		      <div className="modal-body">
-		        <form action="/warehouse/import" method="POST">
+		        <form>
+
+					<div className="form-group" style={{display: "flex"}}>
+		        		<label htmlFor="billCodeInput" style={{flex: "1", marginTop: "10px"}}>請求書コード</label>
+		        		<input id="billCodeInput" style={{flex: "5", marginTop: "5px"}} value={billCode} onChange={handleBillCodeChange} type="text" className="form-control validate" placeholder="請求書コード" required/>
+		        	</div>
+
+					<div className="form-group" style={{display: "flex"}}>
+		        		<label htmlFor="importDateInput" style={{flex: "1", marginTop: "10px"}}>入庫日</label>
+		        		<input id="importDateInput" style={{flex: "5", marginTop: "5px"}} value={importDate} onChange={handleImportDateChange} type="date" className="form-control validate" placeholder="入庫日" required />
+		        	</div>
+
+					<div className="form-group" style={{display: "flex"}}>
+		        		<label htmlFor="suppliertNameInput22" style={{flex: "1", marginTop: "10px"}}>サプライヤー名</label>
+		        		<select name="suppliers" id="suppliertNameInput22" 
+								style={{flex: "5", marginTop: "5px"}} 
+								className="form-control validate"
+								onChange={handleSupplierNameChange} 
+								value={supplierName}>
+		        			{supplierOptions}
+		        		</select>
+		        	</div>
 
 					<div className="form-group" style={{display: "flex"}}>
 		        		<label htmlFor="productNameInput21" style={{flex: "1", marginTop: "10px"}}>製品名</label>
@@ -86,10 +151,11 @@ const ImportModal = (props) => {
 		        		<input id="productQuantityInput2" style={{flex: "5", marginTop: "5px"}} value={productQuantity} onChange={handleProductQuantityChange} type="number" min="1" max="10000" className="form-control validate" placeholder="製品数" required/>
 		        	</div>
 
-		        	<div className="form-group" style={{display: "flex"}}>
-		        		<label htmlFor="importDateInput" style={{flex: "1", marginTop: "10px"}}>入庫日</label>
-		        		<input id="importDateInput" style={{flex: "5", marginTop: "5px"}} value={importDate} onChange={handleImportDateChange} type="date" className="form-control validate" placeholder="入庫日" required />
+					<div className="form-group" style={{display: "flex"}}>
+		        		<label htmlFor="productPriceInput3" style={{flex: "1", marginTop: "10px"}}>単価</label>
+		        		<input id="productPriceInput3" style={{flex: "5", marginTop: "5px"}} value={price} onChange={handleProductPriceChange} type="number" min="1" max="10000000" className="form-control validate" placeholder="単価" />
 		        	</div>
+
 		        </form>
 		      </div>
 		      <div className="modal-footer">
